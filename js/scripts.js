075d1d4552ffc2decbca4c481f71271ef10571b2
Rivaldo Martadinata <63405585+rivaldomrtd@users.noreply.github.com>
2021-10-22 22:07:08 +0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,3 @@
-/*!
-    Title: Dev Portfolio Template
-    Version: 1.2.2
-    Last Change: 03/25/2020
-    Author: Ryan Fitzgerald
-    Repo: https://github.com/RyanFitzgerald/devportfolio-template
-    Issues: https://github.com/RyanFitzgerald/devportfolio-template/issues
-
-    Description: This file contains all the scripts associated with the single-page
-    portfolio website.
-*/
 
 (function($) {
 
